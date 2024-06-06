9203f144b563889fba5f86de54d7ac6d58d70f82
Mate Loncar <mate.loncar@gmail.com>
2024-06-06 14:13:44 +0200

    hacker news

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import HackerNews from './components/HackerNews';
 
 function App() {
+    const [showHackerNews, setShowHackerNews] = useState(false);
+
     useEffect(() => {
         const sections = document.querySelectorAll('section');
         const observer = new IntersectionObserver(entries => {
@@ -43,7 +46,18 @@ function App() {
                 <section id="contact">
                     <Contact />
                 </section>
+                <section>
+                {showHackerNews ? (
+                <HackerNews onClose={() => setShowHackerNews(false)} />
+            ) : (
+                <button className="show-hacker-news-button" onClick={() => setShowHackerNews(true)}>
+                    Show Hacker News
+                </button>
+            )}
+
+                </section>
             </div>
+            
             <Footer />
         </div>
     );
